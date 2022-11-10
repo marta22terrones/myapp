56399847ffe7222f21fe2e71228319e195c16900
mterrones <marta22terrones@gmail.com>
2022-11-10 12:35:58 +0100

    component ExpenseItem created

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+/*
+    * Los componentes siempre comienzan por letra mayúscula
+*/ 
+
+import React  from 'react';
+
+// * Función con mismo nombre que componente
+function ExpenseItem() {
+    return (
+        <h2>Expense Item here</h2>
+    );
+}
+
+// * Se exporta componente para poder usarlo en index.js 
+export default ExpenseItem;
+
